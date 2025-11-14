5b1d48b876f76688e67720fbf6377a7295dfd717
Aker <you@example.com>
2025-11-14 01:04:15 +0100

    Quartz sync: Nov 14, 2025, 1:04 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "",
+    baseUrl: "https://music-of-witches.netlify.app/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
